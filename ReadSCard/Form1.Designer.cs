4d49303df78e984b5d9d7eef97da216c1cfce013
CHENYANLIANG-duke <46186328+CHENYANLIANG-duke@users.noreply.github.com>
2018-12-27 20:49:43 +0800

    加入專案檔案。

---
+++ ReadSCard/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace ReadSCard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnread = new System.Windows.Forms.Button();
+            this.txtlog = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("標楷體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(6, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "卡號 :";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.btnclear);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 123);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(382, 247);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "健保卡資料";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Enabled = false;
+            this.textBox6.Location = new System.Drawing.Point(154, 208);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(208, 22);
+            this.textBox6.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("標楷體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(6, 208);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(120, 21);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "發卡日期 :";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(88, 177);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(79, 22);
+            this.textBox5.TabIndex = 9;
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(287, 17);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(75, 30);
+            this.btnclear.TabIndex = 3;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(88, 145);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(274, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(154, 115);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(208, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(88, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(274, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("標楷體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(6, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 21);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "性別 :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("標楷體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(6, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 21);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "生日 :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("標楷體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(6, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "身分證號碼 :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("標楷體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(6, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "姓名 :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(88, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(274, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnread
+            // 
+            this.btnread.Location = new System.Drawing.Point(12, 12);
+            this.btnread.Name = "btnread";
+            this.btnread.Size = new System.Drawing.Size(112, 105);
+            this.btnread.TabIndex = 2;
+            this.btnread.Text = "讀取健保卡\r\n(Read)";
+            this.btnread.UseVisualStyleBackColor = true;
+            this.btnread.Click += new System.EventHandler(this.btnread_Click);
+            // 
+            // txtlog
+            // 
+            this.txtlog.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtlog.Location = new System.Drawing.Point(128, 12);
+            this.txtlog.Name = "txtlog";
+            this.txtlog.Size = new System.Drawing.Size(264, 22);
+            this.txtlog.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.Location = new System.Drawing.Point(130, 51);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(109, 19);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "列印張數 :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label8.Location = new System.Drawing.Point(170, 83);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 19);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "批號 :";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(236, 48);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(56, 22);
+            this.textBox7.TabIndex = 12;
+            this.textBox7.Text = "1";
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(236, 85);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(56, 22);
+            this.textBox8.TabIndex = 14;
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(318, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 73);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "列印\r\n(print)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 382);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtlog);
+            this.Controls.Add(this.btnread);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "健保卡列印程式";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnread;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.TextBox txtlog;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ReadSCard/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using GS.SCard;
+
+namespace ReadSCard
+{
+
+    public partial class Form1 : Form
+    {
+
+        WinSCard scard = new WinSCard();
+        string str;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            str = System.IO.Directory.GetCurrentDirectory();
+        }
+
+        private void btnread_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                scard.EstablishContext();
+                scard.ListReaders();
+
+                //chioce reader
+                string readerName = scard.ReaderNames[int.Parse("0")];
+
+                txtlog.Text = "Wait for card present...";
+
+                scard.WaitForCardPresent(readerName);
+
+                //建立 Smart Card 連線
+                scard.Connect(readerName);
+
+                Console.WriteLine("ATR: 0x" + scard.AtrString);
+
+                byte[] cmdApdu = { 0x00, 0xA4, 0x04, 0x00, 0x10, 0xD1, 0x58, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x11, 0x00 };
+                byte[] cmdApdu2 = { 0x00, 0xca, 0x11, 0x00, 0x02, 0x00, 0x00 };
+
+
+                byte[] respApdu = new byte[2];
+                byte[] respApdu2 = new byte[59];
+
+                int respLength = respApdu.Length;
+                int respLength2 = respApdu2.Length;
+
+                //下達 Select Profile 檔的 APDU
+                scard.Transmit(cmdApdu, cmdApdu.Length, respApdu, ref respLength);
+
+                //下達讀取Profile指令
+                scard.Transmit(cmdApdu2, cmdApdu2.Length, respApdu2, ref respLength2);
+
+                txtlog.Text = "ok...";
+
+
+                //健保卡ID
+                textBox1.Text = Encoding.Default.GetString(respApdu2, 0, 12);
+                //姓名
+                textBox2.Text = Encoding.Default.GetString(respApdu2, 12, 6);
+                //身份証字號
+                textBox3.Text = Encoding.Default.GetString(respApdu2, 32, 10);
+                //生日
+                textBox4.Text = Encoding.Default.GetString(respApdu2, 43, 2) + "/" + Encoding.Default.GetString(respApdu2, 45, 2) + "/"
+                    + Encoding.Default.GetString(respApdu2, 47, 2);
+                //姓別
+                textBox5.Text = Encoding.Default.GetString(respApdu2, 49, 1);
+                //發卡日期
+                textBox6.Text = Encoding.Default.GetString(respApdu2, 51, 2) + "/" + Encoding.Default.GetString(respApdu2, 53, 2) + "/"
+                    + Encoding.Default.GetString(respApdu2, 55, 2);
+
+            }
+            catch (WinSCardException ex)
+            {
+                Console.WriteLine(ex.WinSCardFunctionName + " 0x" + ex.Status.ToString("X08") + " " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                txtlog.Text = "無卡片 或 卡片接觸有問題...";
+            }
+            finally
+            {
+                scard.Disconnect();
+                scard.ReleaseContext();
+            }
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            txtlog.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            saveconfig();
+            ProcessStartInfo Info2 = new ProcessStartInfo();
+            Info2.FileName = "zebra.bat";//執行的檔案名稱
+            Info2.WorkingDirectory = str + @"\printconfig\";//檔案所在的目錄
+            Process.Start(Info2);
+        }
+
+        private void saveconfig()
+        {
+            StreamWriter sw = new StreamWriter(str + @"\printconfig\YK.TXT",false,Encoding.UTF8);
+
+            sw.Write(textBox3.Text + "," + textBox2.Text + "," + textBox4.Text + "," + textBox5.Text + "," + textBox7.Text + "," + textBox8.Text+","+ DateTime.Now.ToString("yyyy/MM/dd") + "\r\n");
+
+            sw.Close();
+        }
+
+
+    }
+}
